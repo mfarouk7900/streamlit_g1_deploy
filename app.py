064e35469f51031c59da4f15f16454d4e129bf22
mfarouk7900 <mfarouk7900@gmail.com>
2026-05-11 21:23:53 +0200

    change app

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ st.write("this is a simple streamlit app.")
 name=st.text_input("Enter your name:")
 if name:
     st.write(f"Hello,{name}!")
+age=st.slider("select your age:",0,100,25) 
+st.write(f"you are {age} years old.")   
